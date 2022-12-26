c7d079d5343709cfd2b1864c72ada7f02968cf8c
seungminki <fsjki0328@naver.com>
2022-12-26 16:58:15 +0900

    first upload

---
+++ pipeline_dev/settings.py
@@ -0,0 +1,15 @@
+# 작성자 : 기승민(fsjki0328@naver.com)
+# 작성일 : 2022-12-26
+# 내용 : localhost_postgre
+
+DB_SETTINGS = {
+    'localhost_postgre_query' : {
+        # 'location' : 'localhost',
+        # 'engine' : 'postgre',
+        'host' : "127.0.0.1",
+        'port' : "5432",
+        'user' : "postgres",
+        'password' : "0328",
+        'database' : "dvdrental"
+    }
+}
